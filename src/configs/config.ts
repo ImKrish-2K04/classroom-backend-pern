8727f3da9c2fe150b4d647ca70474c7824faa648
Krishna <164477466+ImKrish-2K04@users.noreply.github.com>
2026-06-21 14:34:39 +0530

    feat: integrate better-auth for authentication and update environment configurations

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,8 +4,22 @@ dotenv.config();
 
 const EnvSchema = z.object({
   PORT: z.coerce.number().int().positive(),
-  DATABASE_URL: z.string().min(1, { error: "Please provide database string" }),
+  DATABASE_URL: z
+    .string()
+    .min(1, { error: "DATABASE_URL env variable is required" }),
   FRONTEND_URL: z.url(),
+  ARCJET_KEY: z
+    .string()
+    .min(1, { error: "ARCJET_KEY env variable is required" }),
+  ARCJET_ENV: z
+    .enum(["development", "production", "test"], {
+      error: "ARCJET_ENV env variable is required",
+    })
+    .default("development"),
+  BETTER_AUTH_SECRET: z
+    .string()
+    .min(1, { error: "BETTER_AUTH_SECRET env variable is required" }),
+  BETTER_AUTH_URL: z.url(),
 });
 
 let config = EnvSchema.safeParse(process.env);
